3fd8b0042b0ff97dee24a4a616769c0002a4da34
AlexH001 <ah685@exeter.ac.uk>
2016-11-15 14:25:18 +0000

    This is the previous repository befor it went horribly wrong

---
+++ hello.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main (void)
+{
+
+
+    int n1, n2, Sum;
+    
+    printf("Enter two integers: ");
+
+    // two integers entered by user are stored using scanf
+    scanf("%d %d", &n1, &n2);
+
+    // store sum in variable Sum
+    Sum = n1 + n2;
+	
+	//display the two number in ascending order
+	if(n1<n2)
+	{printf("\n%d %d\n", n1, n2);}
+	else{printf("\n%d %d\n", n2, n1);}
+
+    // display output     
+    printf("%d + %d = %d", n1, n2, Sum);
+	
+	// check if the sum is even or odd, 
+    // and print result to screen
+    if(Sum % 2 == 0) printf("\n%d is even\n", Sum);
+    else printf("\n%d is odd\n", Sum);
+
+    return 0;
+}
