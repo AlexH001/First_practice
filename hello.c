72fe1588bac8ef65029cd9ce90a57fbe66f69dfb
AlexH001 <ah685@exeter.ac.uk>
2016-11-15 14:31:16 +0000

    makign white space between outputs to make it look good

--- hello.c
+++ hello.c
@@ -16,15 +16,15 @@ int main (void)
 	
 	//display the two number in ascending order
 	if(n1<n2)
-	{printf("\n%d %d\n", n1, n2);}
-	else{printf("\n%d %d\n", n2, n1);}
+	{printf("\n\n%d %d\n", n1, n2);}
+	else{printf("\n\n%d %d\n", n2, n1);}
 
     // display output     
-    printf("%d + %d = %d", n1, n2, Sum);
+    printf("\n%d + %d = %d", n1, n2, Sum);
 	
 	// check if the sum is even or odd, 
     // and print result to screen
-    if(Sum % 2 == 0) printf("\n%d is even\n", Sum);
+    if(Sum % 2 == 0) printf("\n\n%d is even\n", Sum);
     else printf("\n%d is odd\n", Sum);
 
 	printf("\n\nhas this changed anythingn?\n\n\n");
